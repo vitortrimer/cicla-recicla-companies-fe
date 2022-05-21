8fe0f359730934047486e1051dbed1d4c8130ed1
Vitor Trimer <vitortrimer@gmail.com>
2022-05-21 15:10:27 -0300

    Add register and router logic

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Login from "./pages/login";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const AppRoutes = () => {
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("user-token")
+    const pathname = window.location.pathname
+    if(!jwt && (pathname !== "/login" && pathname !== "/cadastro")) {
+      window.location.assign("/login")
+    }
+  }, [])
+
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
